264311f7b1352acba01e699677ce3ea8961f4920
Shanting <shantingh2021@student.cis.edu.hk>
2023-07-24 16:34:22 +0800

    clearer code, deploy again

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@
 */
 require('dotenv').config();
 require("@nomiclabs/hardhat-ethers");
-const { API_URL, PRIVATE_KEY, PRIVATE_KEY_2, PRIVATE_KEY_3 } = process.env;
+const { API_URL, OWNER_PRIVATE_KEY, PRIVATE_KEY_2, PRIVATE_KEY_3 } = process.env;
 module.exports = {
    solidity: "0.8.18",
    defaultNetwork: "sepolia",
@@ -11,7 +11,7 @@ module.exports = {
       hardhat: {},
       sepolia: {
          url: API_URL,
-         accounts: [`0x${PRIVATE_KEY}`, `0x${PRIVATE_KEY_2}`, `0x${PRIVATE_KEY_3}`]
+         accounts: [`0x${OWNER_PRIVATE_KEY}`, `0x${PRIVATE_KEY_2}`, `0x${PRIVATE_KEY_3}`]
       }
    },
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,20 @@
-const PUBLIC_KEY = process.env.PUBLIC_KEY
+const OWNER_PUBLIC_KEY = process.env.OWNER_PUBLIC_KEY
 async function main() {
+    //EAvatar Deployment -------------------------------------------------------
     const EAvatar = await ethers.getContractFactory("EAvatar")
   
     // Start deployment, returning a promise that resolves to a contract object
-    const eAvatar = await EAvatar.deploy(PUBLIC_KEY)
+    const eAvatar = await EAvatar.deploy(OWNER_PUBLIC_KEY)
     await eAvatar.deployed()
-    console.log("Contract deployed to address:", eAvatar.address)
+    console.log("EAvatar deployed to address:", eAvatar.address)
+
+    //Eco Item Deployment ------------------------------------------------------
+    const EcoItems = await ethers.getContractFactory("EcoItems")
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const ecoItems = await EcoItems.deploy()
+    await ecoItems.deployed()
+    console.log("EcoItems deployed to address:", ecoItems.address)
   }
   
   main()
